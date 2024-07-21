0da4f35c2ccc6790dd82400819ef7de9470391f6
ARYAN TOMAR <63942312+aryan-py@users.noreply.github.com>
2024-07-21 19:58:16 +0530

    Update urls.py

--- inventory_management/inventory/urls.py
+++ inventory_management/inventory/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Index, SignUpView, Dashboard, AddItem, EditItem, DeleteItem 
+from .views import Index, SignUpView, Dashboard, AddItem, EditItem, DeleteItem, search 
 # INdex is not the file name rather points to the class name in views.py
 from django.contrib.auth import views as auth_views
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('signup/',SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
+    path('searchbar/', search.as_view(), name='search')
 ]
